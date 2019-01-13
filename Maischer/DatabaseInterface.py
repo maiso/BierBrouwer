be7edf44f5be146605102c0d6ed3d130188967a2
maiso <michielaiso@hotmail.com>
2019-01-13 23:08:29 +0100

    More work on databases

--- Maischer/DatabaseInterface.py
+++ Maischer/DatabaseInterface.py
@@ -4,7 +4,7 @@ import threading
 import time
 import sqlite3
 
-#ID, NAME, BrewDate, CalibrationId,MaischSchematicId
+#ID, NAME, BrewDate, CalibrationId,MashingSchematicId
 class DatabaseInterface():
     def __init__(self, databaseName):
         self.conn = sqlite3.connect(databaseName)
@@ -13,31 +13,6 @@ class DatabaseInterface():
         # lambda c, r: dict([(col[0], r[idx]) for idx, col in enumerate(c.description)])
 
         self.createTables()
-
-    def getAllBrewages(self):
-        self.c.execute('''
-            SELECT * FROM Brewages
-            ''')
-        self.rows = self.c.fetchall()
-
-        print (self.rows)
-        #print (self.rows[0]['NAME'])
-        print([dict(row) for row in self.rows])
-        jsonRet = json.dumps([dict(row) for row in self.rows])
-        print (jsonRet)   
-        return jsonRet
-
-    def newBrewages(self, name, date, CalibrationId,MaischSchematic):
-        self.c.execute('''
-            INSERT INTO Brewages(
-               NAME,
-               BrewDate,
-               CalibrationId,
-               MaischSchematicId
-            ) 
-            VALUES(?,?,?,?);
-            ''', (name,date,CalibrationId,MaischSchematic))
-        self.conn.commit()
 
     def getCalibration(self,BrewageId):
         t = (BrewageId,)
@@ -54,9 +29,10 @@ class DatabaseInterface():
 
     def createTables(self):
         self.createCalibrationTable()
+        self.createMashingSchematicTable()
         self.createSetpointsTable()
+        self.createBoilingSchematicTable()
         self.createHopMomentsTable()
-        self.createMaischSchematicTable()
         self.createBrewagesTable()
         self.createMeasurementsTable()
 
@@ -76,49 +52,146 @@ class DatabaseInterface():
             );
             ''')
 
+#########################################################################
+### MashingSchematic
+#########################################################################
+    def createMashingSchematicTable(self):
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS MashingSchematic(
+               ID    INTEGER PRIMARY KEY AUTOINCREMENT,
+               Name  TEXT  NOT NULL          
+            );
+            ''')
+
+    def newMashingSchematic(self, name):
+        self.c.execute('''
+            INSERT INTO MashingSchematic(
+               Name
+            ) 
+            VALUES(?);
+            ''', (name))
+        self.conn.commit()
+
+### SetPoints
     def createSetpointsTable(self):
         self.c.execute('''
             CREATE TABLE IF NOT EXISTS Setpoints(
                ID         INTEGER PRIMARY KEY AUTOINCREMENT,
                StartTime  TEXT  NOT NULL,               
-               SetPoint   REAL
+               SetPoint   REAL ,
+               MashingSchematicId INT NOT NULL,
+               FOREIGN KEY(MashingSchematicId) REFERENCES MashingSchematic(ID)
             );
             ''')
 
+    def newSetpoint(self, StartTime, SetPoint, MashingSchematicId):
+        self.c.execute('''
+            INSERT INTO Setpoints(
+               StartTime,
+               SetPoint,
+               MashingSchematicId
+            ) 
+            VALUES(?,?,?);
+            ''', (StartTime, SetPoint, MashingSchematicId))
+        self.conn.commit()
+
+#########################################################################
+### BoilingSchematic
+#########################################################################
+    def createBoilingSchematicTable(self):
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS BoilingSchematic(
+               ID                 INTEGER PRIMARY KEY AUTOINCREMENT,
+               BoilingTime        INTEGER,
+               BoilingTemperature INTEGER
+            );
+            ''')
+
+    def newBoilingSchematic(self, BoilingTime,BoilingTemperature ):
+        self.c.execute('''
+            INSERT INTO Brewages(
+               BoilingTime,
+               BoilingTemperature
+            ) 
+            VALUES(?,?);
+            ''', (BoilingTime,BoilingTemperature))
+        self.conn.commit()
+
+### HopMoments
     def createHopMomentsTable(self):
         self.c.execute('''
             CREATE TABLE IF NOT EXISTS HopMoments(
                ID             INTEGER PRIMARY KEY AUTOINCREMENT,
                StartTime      TEXT  NOT NULL,               
-               HOPNAME        TEXT  NOT NULL,
-               AmountInGrams  REAL
+               Hopname        TEXT  NOT NULL,
+               AmountInGrams  REAL,
+               BoilingSchematicId INT NOT NULL,
+               FOREIGN KEY(BoilingSchematicId) REFERENCES BoilingSchematic(ID)
             );
             ''')
 
-    def createMaischSchematicTable(self):
+    def newHopMoments(self, StartTime, Hopname, AmountInGrams, BoilingSchematicId):
         self.c.execute('''
-            CREATE TABLE IF NOT EXISTS MaischSchematic(
-               ID    INTEGER PRIMARY KEY AUTOINCREMENT,
-               NAME  TEXT  NOT NULL,               
-               SetpointsId INT,
-               HopMomentsId INT,
-               FOREIGN KEY(SetpointsId) REFERENCES Setpoints(ID),
-               FOREIGN KEY(HopMomentsId) REFERENCES HopMoments(ID)
-            );
-            ''')
+            INSERT INTO HopMoments(
+               StartTime,
+               Hopname,
+               AmountInGrams,
+               BoilingSchematicId
+            )
+            VALUES(?,?,?);
+            ''', (StartTime, Hopname, AmountInGrams, BoilingSchematicId))
+        self.conn.commit()
 
+#########################################################################
+### Brewages
+#########################################################################
     def createBrewagesTable(self):
         self.c.execute('''
             CREATE TABLE IF NOT EXISTS Brewages(
-               ID        INTEGER PRIMARY KEY AUTOINCREMENT,
-               NAME      TEXT  NOT NULL UNIQUE,
-               BrewDate  TEXT  NOT NULL UNIQUE,
-               CalibrationId INT,
-               MaischSchematicId INT,
+               ID                 INTEGER PRIMARY KEY AUTOINCREMENT,
+               NAME               TEXT  NOT NULL UNIQUE,
+               BrewDate           TEXT  NOT NULL UNIQUE,
+               MashingStartTime   TEXT,
+               MashingStopTime    TEXT,
+               BoilingStartTime   TEXT,
+               BoilingStopTime    TEXT,
+               CalibrationId      INT,
+               MashingSchematicId INT,
                FOREIGN KEY(CalibrationId) REFERENCES Calibration(ID),
-               FOREIGN KEY(MaischSchematicId) REFERENCES MaischSchematic(ID)
+               FOREIGN KEY(MashingSchematicId) REFERENCES MashingSchematic(ID)
             );
             ''')
+
+    def getAllBrewages(self):
+        self.c.execute('''SELECT * FROM Brewages''')
+        self.rows = self.c.fetchall()
+
+        print (self.rows)
+        #print (self.rows[0]['NAME'])
+        print([dict(row) for row in self.rows])
+        jsonRet = json.dumps([dict(row) for row in self.rows])
+        print (jsonRet)   
+        return jsonRet
+
+    def newBrewage(self, name, date, CalibrationId,MashingSchematic):
+        self.c.execute('''
+            INSERT INTO Brewages(
+               NAME,
+               BrewDate,
+               CalibrationId,
+               MashingSchematicId
+            ) 
+            VALUES(?,?,?,?);
+            ''', (name,date,CalibrationId,MashingSchematic))
+        self.conn.commit()
+
+        self.c.execute('''SELECT ID FROM Brewages WHERE NAME = ?''',(name,))
+        return self.c.fetchone()['ID']
+
+
+#########################################################################
+### Measurements
+#########################################################################
     def createMeasurementsTable(self):
         self.c.execute('''
             CREATE TABLE IF NOT EXISTS Measurements(
@@ -142,7 +215,8 @@ if __name__ == "__main__":
     dbInterface = DatabaseInterface(databaseName)
     jsonRet = dbInterface.getAllBrewages()
     print (jsonRet) 
-    dbInterface.newBrewages("TestBrew2",dateTime,None,None)        
+    newId = dbInterface.newBrewage(dateTime,dateTime,None,None)        
+    print ("newId " + str(newId))
     #except Exception as e:
     #    print (str(e))
     
